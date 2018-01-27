51d7fc323418d6c2e87db8774ae5068b33c18d0e
unknown <951127336@qq.com>
2018-01-27 20:40:19 +0800

    CNN practice

--- CNN.py
+++ CNN.py
@@ -9,7 +9,7 @@ def weight_variable(shape):
     return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1,shap=shape)
+    initial = tf.constant(0.1,shape=shape)
     return tf.Variable(initial)
 
 def conv2d(x,W):
